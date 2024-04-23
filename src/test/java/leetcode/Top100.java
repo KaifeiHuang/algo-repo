ddd772dfea28b2a1a0988bc85d48bcbd4c566f9c
huangkaifei <kaifeibandao@163.com>
2024-04-23 15:15:43 +0800

    top43: 字符串相乘 解法：竖版相乘，求尾数，求进位和

--- src/test/java/leetcode/Top100.java
+++ src/test/java/leetcode/Top100.java
@@ -18,8 +18,42 @@ public class Top100 {
 
     }
 
-
     public String multiply1(String s1, String s2) {
+        // 临界条件
+        if ( "0".equals(s1) ||  "0".equals(s2)) return "0";
+        // 竖版求解
+        int m = s1.length();
+        int n = s2.length();
+        int[] ans = new int[m + n];
+        for (int i = m-1; i >=0; i--) {
+            int a = s1.charAt(i) - '0';
+            for (int j = n-1; j >=0; j--) {
+                int b = s2.charAt(j) - '0';
+                int sum = ans[i+j+1] + a*b;
+                // 尾数求和
+                ans[i+j+1] = sum %10;
+                // 进位加和
+                ans[i+j] += sum /10;
+            }
+        }
+        // 结果输出
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ans.length; i++) {
+            if (i==0 && ans[i] == 0) continue;
+            sb.append(ans[i]);
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     * top43： 43. 字符串相乘  https://leetcode.cn/problems/multiply-strings/description/
+     *
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public String multiply(String s1, String s2) {
         // 临界条件
         if (s1.equals("0") || s2.equals("0"))
             return "0";
@@ -50,21 +84,6 @@ public class Top100 {
         }
         return sb.toString();
 
-    }
-
-    /**
-     * top43： 43. 字符串相乘  https://leetcode.cn/problems/multiply-strings/description/
-     *
-     * @param num1
-     * @param num2
-     * @return
-     */
-    public String multiply(String num1, String num2) {
-        BigInteger ba = new BigInteger(num1);
-        BigInteger bb = new BigInteger(num2);
-        BigInteger multiply = ba.multiply(bb);
-        System.out.println(multiply.intValue());
-        return multiply.intValue() + "";
     }
 
 
