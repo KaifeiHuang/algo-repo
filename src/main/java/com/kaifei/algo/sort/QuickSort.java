6315467cfcf02503b347580b97f447bbd7288f70
huangkaifei <kaifeibandao@163.com>
2024-04-24 10:51:41 +0800

    quick sort algo

--- src/main/java/com/kaifei/algo/sort/QuickSort.java
+++ src/main/java/com/kaifei/algo/sort/QuickSort.java
@@ -5,7 +5,7 @@ public class QuickSort {
 
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 8, 3, 9, 4, 5, 7};
+        int[] arr = new int[]{1, 0, 8, -3, 9, 4, 5, 7};
         System.out.println("before:");
         printArr(arr);
 
@@ -15,50 +15,53 @@ public class QuickSort {
         printArr(arr);
     }
 
-    private static void printArr(int[] arr) {
-        for (int j : arr) {
-            System.out.print(j + " ");
-        }
-    }
 
     public static void quickSort(int[] arr, int low, int high) {
         if (low >= high) return;
 
+        // 找基准元素
+        int pivot = arr[high];
 
-        // step1: choose pivot
-        int pivot = high;
+        // 分区
+        int lp = partition(arr, low, high, pivot);
 
-        int leftPointer = low;
-        int rightPointer = high;
-
-        // step2: partition
-        while (leftPointer < rightPointer) {
-            // leftPointer往右侧移动，直到它位置元素比pivot位置大
-            while (arr[leftPointer] <= arr[pivot] && leftPointer < rightPointer) {
-                leftPointer++;
-            }
-
-
-            // rightPointer往左侧移动，直到它位置元素比pivot位置小
-            while (arr[rightPointer] >= arr[pivot] && leftPointer < rightPointer) {
-                rightPointer--;
-            }
-
-            // 交换lp和rp
-            swap(arr, leftPointer, rightPointer);
-
-        }
-        // 将lp与pivot进行交换
-        swap(arr, leftPointer, pivot);
-
-        quickSort(arr, low, leftPointer - 1);
-        quickSort(arr, leftPointer + 1, high);
-
+        // 递归排序
+        quickSort(arr, low, lp - 1);
+        quickSort(arr, lp + 1, high);
     }
+
+    private static int partition(int[] arr, int low, int high, int pivot) {
+        int lp = low;
+        int rp = high;
+
+        while (lp < rp) {
+            // lp往右移动
+            while (arr[lp] <= pivot && lp < rp) {
+                lp++;
+            }
+            //rp往左移动
+            while (arr[rp] >= pivot && lp < rp) {
+                rp--;
+            }
+            // lp比pivot大，rp比pivot小，交换
+            swap(arr, lp, rp);
+        }
+        // lp到rp位置，lp与pivot交换
+        swap(arr, lp, high);
+        return lp;
+    }
+
 
     private static void swap(int[] arr, int leftPointer, int rightPointer) {
         int temp = arr[rightPointer];
         arr[rightPointer] = arr[leftPointer];
         arr[leftPointer] = temp;
+    }
+
+
+    private static void printArr(int[] arr) {
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
     }
 }
